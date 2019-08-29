67ffa613d720ec7e900b7132013fb3656571d5cf
Victor Wardi <victorcaetano@live.com>
2019-08-29 08:00:55 -0300

    Interface Default Method Tests.

--- src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/ControllerTests.java
@@ -1,7 +1,15 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTests {
+
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Does something before all controller tests.");
+    }
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/ModelTests.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/ModelTests.java
@@ -1,7 +1,15 @@
 package guru.springframework.sfgpetclinic.model;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
 @Tag("model")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public interface ModelTests {
+
+    @BeforeAll
+    default void beforeAll(){
+        System.out.println("Does something before all model tests.");
+    }
 }
